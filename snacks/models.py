cb49a9c185849e16f8ab8c5618984daf37cb21e6
Caleb Hemphill <calebhemp@gmail.com>
2024-02-14 18:59:44 -0500

    Create snack model

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+class Snack(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('snack_detail', kwargs={'pk': self.pk})
