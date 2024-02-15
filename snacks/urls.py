dfb8b4d2546c1aed09cbca9d45e64dd51997672f
Caleb Hemphill <calebhemp@gmail.com>
2024-02-14 19:13:05 -0500

    Create CRUD views

---
+++ snacks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import SnackListView, SnackDetailView, SnackCreateView, SnackUpdateView, SnackDeleteView
+
+
+urlpatterns = [
+    path('', SnackListView.as_view(), name='snack_list'),
+    path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail'),
+    path('create/', SnackCreateView.as_view(), name='snack_detail'),
+    path('<int:pk>/update/', SnackUpdateView.as_view(), name='snack_detail'),
+    path('<int:pk>/delete/', SnackDeleteView.as_view(), name='snack_detail'),
+]
